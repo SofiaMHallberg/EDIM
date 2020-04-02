a3da0e92d8db1d07ca566598ed6b7a83ce493956
ChanonBorg <60186836+ChanonBorg@users.noreply.github.com>
2020-04-02 13:35:45 +0200

    Update ServerController.java

--- src/server/ServerController.java
+++ src/server/ServerController.java
@@ -4,11 +4,12 @@ import java.util.HashMap;
 
 /**
  * This class handles the logic of the in and out coming object.
- * @autor Sofia Hallberg & Chanon Borgström.
+ *
  * @version 1.0
+ * @autor Sofia Hallberg & Chanon Borgström.
  */
 
-public class ServerController extends Thread{
+public class ServerController extends Thread {
     private Buffer<User> onLineBuffer;
     private Buffer<User> sendUserBuffer;
     private Buffer<User> sendNewUserBuffer;
@@ -20,32 +21,33 @@ public class ServerController extends Thread{
 
     /**
      * Constructs all the buffers and servers and HashMaps that is needed.
+     *
      * @param port the received port number.
      */
     public ServerController(int port) {
-        onLineBuffer=new Buffer();
-        socketHashMap=new HashMap();
-        sendUserBuffer=new Buffer();
-        sendNewUserBuffer=new Buffer();
-        sendNewActivityBuffer=new Buffer();
-        connectionServer=new ConnectionServer(this, port, socketHashMap, onLineBuffer);
-        communicationServer=new CommunicationServer(port, socketHashMap, sendUserBuffer, sendNewUserBuffer, sendNewActivityBuffer);
-        userRegister=new UserRegister();
+        onLineBuffer = new Buffer();
+        socketHashMap = new HashMap();
+        sendUserBuffer = new Buffer();
+        sendNewUserBuffer = new Buffer();
+        sendNewActivityBuffer = new Buffer();
+        connectionServer = new ConnectionServer(this, port, socketHashMap, onLineBuffer);
+        communicationServer = new CommunicationServer(port, socketHashMap, sendUserBuffer, sendNewUserBuffer, sendNewActivityBuffer);
+        userRegister = new UserRegister();
     }
 
     /**
      * Checks whether the User who recently logged in is a new user or not.
+     *
      * @param user the logged in User.
      * @return an updated User.
      */
     public User checkOnlineUsers(User user) {
-       // for(User u:userRegister.getUserList()) {
-        for(int i = 0; i<userRegister.getUserList().size(); i++) {
-            if(userRegister.getUserList().get(i).equals(user)) {
-                user=userRegister.getUserList().get(i);
+        // for(User u:userRegister.getUserList()) {
+        for (int i = 0; i < userRegister.getUserList().size(); i++) {
+            if (userRegister.getUserList().get(i).equals(user)) {
+                user = userRegister.getUserList().get(i);
                 user.setUserType(UserType.SENDUSER);
-            }
-            else {
+            } else {
                 user.setUserType(UserType.SENDWELCOME);
                 System.out.println("checkOnlineUsers - else-satsen");
             }
@@ -55,10 +57,11 @@ public class ServerController extends Thread{
 
     /**
      * Checks an updated User object.
-     * @param updatedUser
+     *
+     * @param updatedUser the updated user object who need to be checked.
      */
     public void checkUpdatedUser(User updatedUser) {
-        UserType userType=updatedUser.getUserType();
+        UserType userType = updatedUser.getUserType();
         System.out.println("UserType: " + userType);
         switch (userType) {
             case SENDUSER:
@@ -82,7 +85,7 @@ public class ServerController extends Thread{
      * Receives a User object from the online buffer and checks it value.
      */
     public void run() {
-        while(true) {
+        while (true) {
             try {
                 User user = onLineBuffer.get();
                 UserType userType = user.getUserType();
@@ -90,7 +93,7 @@ public class ServerController extends Thread{
                 switch (userType) {
                     case LOGIN:
                         userRegister.getUserList().add(user);
-                        User updatedUser=checkOnlineUsers(user);
+                        User updatedUser = checkOnlineUsers(user);
                         checkUpdatedUser(updatedUser);
                         testActivity();
                         System.out.println("ServerController: 2 " + user.getUserName());
@@ -110,7 +113,7 @@ public class ServerController extends Thread{
     }
 
     public static void main(String[] args) {
-        ServerController controller=new ServerController(3343);
+        ServerController controller = new ServerController(3343);
         controller.start();
     }
 
